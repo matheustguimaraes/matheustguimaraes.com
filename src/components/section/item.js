a77c119bc9dee35a0dc0f3994e48ba6853d88e9c
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-12 00:43:24 -0300

    Change to Link

--- src/components/section/item.js
+++ src/components/section/item.js
@@ -1,11 +1,12 @@
 import React from "react"
 
-import LinkBlank from "../link-blank"
+// import LinkBlank from "../link-blank"
 import classes from "./item.module.css"
+import { Link } from "gatsby"
 
 const Item = props => (
   <h3>
-    <LinkBlank href={props.href}>{props.title}</LinkBlank>
+    <Link className={classes.LinkA} to={props.href}>{props.title}</Link>
     <p className={classes.Datetime}>{props.intro ? props.intro : ""}</p>
   </h3>
 )
