55d0134a9613f9a51acfd235b4bbea862da366f8
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-13 18:43:46 -0300

    Rename post folder to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
+        path: `${__dirname}/src/content`,
       },
     },
     `gatsby-transformer-remark`,
