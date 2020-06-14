b124aaa17e8b36d191045630501e2f8ae48ca1d4
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-13 22:18:32 -0300

    Add landing page

---
+++ src/components/landingBio.js
@@ -0,0 +1,67 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "@emotion/styled"
+
+import Emoji from "../components/emoji"
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const OuterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  height: 78vh;
+`
+
+const Description = styled.p`
+  padding: 0;
+  margin-bottom: 1rem;
+  font-size: 1.4rem;
+`
+
+const NameHeader = styled.h1`
+  font-size: 3.5rem;
+  margin-bottom: 0;
+`
+
+const LandingBio = () => (
+  <StaticQuery
+    query={graphql`
+      query LandingSiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            subtitle
+          }
+        }
+      }
+    `}
+    render={data => (
+      <OuterContainer>
+        <Container>
+          <NameHeader>{data.site.siteMetadata.title}</NameHeader>
+          <Description>
+            Hey! I am a <b>{data.site.siteMetadata.subtitle}</b> from Brazil.{` `}
+            <Emoji symbol="🇧🇷" />
+          </Description>
+        </Container>
+      </OuterContainer>
+    )}
+  />
+)
+
+NameHeader.propTypes = {
+  siteTitle: PropTypes.string,
+  subtitle: PropTypes.string,
+}
+
+NameHeader.defaultProps = {
+  siteTitle: ``,
+  subtitle: ``,
+}
+
+export default LandingBio
