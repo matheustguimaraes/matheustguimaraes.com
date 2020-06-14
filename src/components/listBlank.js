e35bdb4726f60812c130365a7a10c56d52b1554b
Matheus Guimaraes <matheusguimr@gmail.com>
2020-06-13 22:15:51 -0300

    Remove css

--- src/components/listBlank.js
+++ src/components/listBlank.js
@@ -1,14 +1,7 @@
 import React from "react"
 
-import classes from "./link.module.css"
-
 const LinkBlank = props => (
-  <a
-    className={classes.LinkA}
-    target="_blank"
-    rel="noopener noreferrer"
-    href={props.href}
-  >
+  <a target="_blank" rel="noopener noreferrer" href={props.href}>
     {props.children}
   </a>
 )
