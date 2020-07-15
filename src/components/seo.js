1b6f9d89fb0e89ffc85135b9580695eaf7ea4d5d
Matheus Guimaraes <matheusguimr@gmail.com>
2020-07-14 22:32:31 -0300

    Add high voltage img as link thumbnail

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,8 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import ImgThumbnail from "./image"
+
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -50,6 +52,10 @@ function SEO({ description, lang, meta, keywords, title }) {
         {
           property: `og:type`,
           content: `website`,
+        },
+        {
+          property: `og:image`,
+          content: ImgThumbnail,
         },
         {
           name: `twitter:card`,
