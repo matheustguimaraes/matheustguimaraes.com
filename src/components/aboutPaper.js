3cb5ff7e71faf800625275cc02b0f5dfaf1abd79
Matheus Guimaraes <matheusguimr@gmail.com>
2020-11-19 18:42:50 -0300

    Create aboutPaper.js

---
+++ src/components/aboutPaper.js
@@ -0,0 +1,40 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+import LinkBlank from "./linkBlank"
+
+const aboutSection = props => (
+  <div>
+    <GatsbyLink href={props.href}>
+      <MarkerHeader>{props.title} </MarkerHeader>
+    </GatsbyLink>
+    <div>
+      <PostDate>{props.date}</PostDate>
+    </div>
+    <p>{props.children ? props.children : ""}</p>
+  </div>
+)
+
+const PostDate = styled.h5`
+  display: inline;
+  color: #606060;
+`
+
+const MarkerHeader = styled.h5`
+  display: inline;
+  border-radius: 1em 0 1em 0;
+  // background-image: linear-gradient(
+  //   -100deg,
+  //   rgba(120, 255, 0, 0.15),
+  //   rgba(255, 0, 0, 80) 100%,
+  //   rgba(255, 0, 0, 80)
+  // );
+`
+
+const GatsbyLink = styled(LinkBlank)`
+  margin-left: 5px;
+  text-decoration: none!important;
+  color: inherit;
+`
+
+export default aboutSection
